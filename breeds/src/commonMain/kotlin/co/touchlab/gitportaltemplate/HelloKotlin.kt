d7eed6a33aab4c20e6af052fe9015d408e0b4063
Stefan Nitu <snitu@specialized.com>
2026-05-14 15:17:16 +0300

    one more

--- breeds/src/commonMain/kotlin/co/touchlab/gitportaltemplate/HelloKotlin.kt
+++ breeds/src/commonMain/kotlin/co/touchlab/gitportaltemplate/HelloKotlin.kt
@@ -2,6 +2,6 @@ package co.touchlab.gitportaltemplate
 
 class HelloKotlin {
     fun sayHello(): String {
-        return "IOS side — apple this time"
+        return "IOS side — apple this time, one more change"
     }
 }
